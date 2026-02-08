e33cc83b954408e215168b6e616f35aca8d14fa0
nikolalaskov <nikola@laskov.us>
2026-02-08 10:27:41 +0200

    update filter for breakpoints, change icons ant their location to be shown on both new and old ui

---
+++ src/main/kotlin/bg/laskov/breakbeat/BreakpointSoundStatusBarWidgetFactory.kt
@@ -0,0 +1,18 @@
+package bg.laskov.breakbeat
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.util.ui.JBUI
+
+class BreakpointSoundStatusBarWidgetFactory : StatusBarWidgetFactory {
+
+    override fun getId(): String = "BreakpointSoundStatus"
+
+    override fun getDisplayName(): String = "Breakpoint Sound"
+
+    override fun isAvailable(project: Project): Boolean = true
+
+    override fun createWidget(project: Project): StatusBarWidget =
+        BreakpointSoundStatusBarWidget(project)
+}
--- src/main/kotlin/bg/laskov/breakbeat/ToggleBreakpointSoundAction.kt
+++ src/main/kotlin/bg/laskov/breakbeat/ToggleBreakpointSoundAction.kt
@@ -3,19 +3,13 @@ package bg.laskov.breakbeat
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 import javax.swing.ImageIcon
 
 class ToggleBreakpointSoundAction : AnAction("Toggle Breakpoint Sound") {
 
     companion object {
-        // Use emoji as icon text (works as icon text in toolbar)
-        private val ENABLED_ICON: Icon by lazy {
-            ImageIcon(ToggleBreakpointSoundAction::class.java.getResource("/icons/green.svg.png"))
-        }
-        private val DISABLED_ICON: Icon by lazy {
-            ImageIcon(ToggleBreakpointSoundAction::class.java.getResource("/icons/red.svg.png"))
-        }
         var enabled = true
     }
 
@@ -28,11 +22,5 @@ class ToggleBreakpointSoundAction : AnAction("Toggle Breakpoint Sound") {
         if(!enabled) {
             soundPlayer.stop()
         }
-    }
-
-    override fun update(e: AnActionEvent) {
-        // Swap icon dynamically
-        e.presentation.icon = if (enabled) ENABLED_ICON else DISABLED_ICON
-        e.presentation.text = "" // optional: no text, just icon
     }
 }
